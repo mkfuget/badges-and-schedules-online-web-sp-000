2094a1091d29d6ed16c5855d136660110c9bd034
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:35:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(names_array)
 end
 
 def assign_rooms(names_array)
-  return names_array.map.with_index{|x, i| "Hello, #{x}! You'll be assigned to room #{i}!"}
+  return names_array.map.with_index{|x, i| "Hello, #{x}! You'll be assigned to room #{i+1}!"}
 end
 
 def printer(names_array)
